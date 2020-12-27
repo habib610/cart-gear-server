4bf8a43e02c28e3c4bb02c4af953844d667084f4
habib610 <habib54562@gmail.com>
2020-12-27 11:52:24 +0600

    sample user and admin

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import userRouter from './router/userRouter.js';
 
 const app = express();
+mongoose.connect( process.env.MONGODB_URL || 'mongodb://localhost/cartgear', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
 
 const port = process.env.PORT || 4000;
 
@@ -21,9 +28,14 @@ app.get('/api/products', (req, res) => {
     res.send(data.products)
 })
 
-
 app.get('/', (req, res) => {
     res.send('Server is Running')
+})
+
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next)=> {
+    res.status(500).send({message: err.message})
 })
 
 app.listen(port, ()=> {
