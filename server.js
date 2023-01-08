a2503da2807246f822f90559d25402235fabf57b
habib610 <habib54562@gmail.com>
2023-01-08 23:26:21 +0600

    cors-updated

--- server.js
+++ server.js
@@ -1,53 +1,46 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import userRouter from './router/userRouter.js';
-import productRouter from './router/productRouter.js';
-import orderRouter from './router/orderRouter.js';
-import dotenv from 'dotenv';
-
-dotenv.config()
-
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import orderRouter from "./router/orderRouter.js";
+import productRouter from "./router/productRouter.js";
+import userRouter from "./router/userRouter.js";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-
-mongoose.connect( process.env.MONGODB_URL , {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
-})
-
-
-const connection = mongoose.connection;
-connection.once("open", ()=> {
-  // we're connected!
-  console.log("Database Connected Successfully");
+    useCreateIndex: true,
 });
 
-
+const connection = mongoose.connection;
+connection.once("open", () => {
+    // we're connected!
+    console.log("Database Connected Successfully");
+});
 
 const port = process.env.PORT || 4000;
 
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/users", userRouter);
+app.get("/api/config/paypal", (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
+app.get("/", (req, res) => {
+    res.send("Server is Running");
+});
 
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
-app.use('/api/users', userRouter);
-app.get('/api/config/paypal', (req, res)=> {
-    res.send(process.env.PAYPAL_CLIENT_ID )
-})
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});
 
-app.get('/', (req, res) => {
-    res.send('Server is Running')
-})
-
-
-app.use((err, req, res, next)=> {
-    res.status(500).send({message: err.message})
-})
-
-app.listen(port, ()=> {
-    console.log("listening port", port)
-})
+app.listen(port, () => {
+    console.log("listening port", port);
+});
